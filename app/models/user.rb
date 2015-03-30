8328b5fce0261a1b2c53d4b8b46aaf75e1efc454
Carol Nichols <carol.nichols@gmail.com>
2015-03-30 19:41:20 -0400

    Set the token and secret every time in case they've changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(provider: auth['provider'], uid: auth['uid']).first || create_from_omniauth(auth)
+    user = where(provider: auth['provider'], uid: auth['uid']).first || create_from_omniauth(auth)
+    user.token  = auth['credentials']['token']
+    user.secret = auth['credentials']['secret']
+    user.save!
+    user
   end
 
   def self.create_from_omniauth(auth)
@@ -8,8 +12,6 @@ class User < ActiveRecord::Base
       user.provider = auth['provider']
       user.uid      = auth['uid']
       user.username = auth['info']['username']
-      user.token    = auth['credentials']['token']
-      user.secret   = auth['credentials']['secret']
     end
   end
 end
