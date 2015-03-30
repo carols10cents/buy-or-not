0c5468963e0f5b5392718640d89f7bf2d93a6a13
Carol Nichols <carol.nichols@gmail.com>
2015-03-29 21:50:52 -0400

    Store users' oauth info on first login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
-    where(provider: auth["provider"], uid: auth["uid"]).first || create_from_omniauth(auth)
+    where(provider: auth['provider'], uid: auth['uid']).first || create_from_omniauth(auth)
   end
 
   def self.create_from_omniauth(auth)
     create! do |user|
-      user.provider = auth["provider"]
-      user.uid      = auth["uid"]
-      user.username = auth["info"]["username"]
+      user.provider = auth['provider']
+      user.uid      = auth['uid']
+      user.username = auth['info']['username']
+      user.token    = auth['credentials']['token']
+      user.secret   = auth['credentials']['secret']
     end
   end
 end
